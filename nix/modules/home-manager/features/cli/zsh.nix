8a768262fc7329d41f74029c88d70cefc0cf7207
Oliver Davies <oliver@oliverdavies.dev>
2024-12-28 23:24:05 +0000

    Update syntax highlighting

--- nix/modules/home-manager/features/cli/zsh.nix
+++ nix/modules/home-manager/features/cli/zsh.nix
@@ -4,7 +4,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
 
     cdpath = [ "~/Code" ];
     dotDir = ".config/zsh";
