33c4da6f92c97431d4a53fc12c5b3beb266e1739
Oliver Davies <oliver@oliverdavies.dev>
2025-01-01 19:15:04 +0000

    lemp11: restore normal Caps Lock key

--- nix/hosts/lemp11/configuration.nix
+++ nix/hosts/lemp11/configuration.nix
@@ -106,28 +106,6 @@
     };
   };
 
-  # Make Caps lock work as an Escape key on press and Ctrl on hold.
-  services.interception-tools =
-    let
-      dfkConfig = pkgs.writeText "dual-function-keys.yaml" ''
-        MAPPINGS:
-          - KEY: KEY_CAPSLOCK
-            TAP: KEY_ESC
-            HOLD: KEY_LEFTCTRL
-      '';
-    in
-    {
-      enable = true;
-      plugins = pkgs.lib.mkForce [ pkgs.interception-tools-plugins.dual-function-keys ];
-      udevmonConfig = ''
-        - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c ${dfkConfig} | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
-          DEVICE:
-            NAME: "AT Translated Set 2 keyboard"
-            EVENTS:
-              EV_KEY: [[KEY_CAPSLOCK, KEY_ESC, KEY_LEFTCTRL]]
-      '';
-    };
-
   services.gvfs.enable = true;
 
   programs.gnupg.agent = {
