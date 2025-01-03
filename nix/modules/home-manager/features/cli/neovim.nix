aa666a2391504313ba042c1fc143c8fc9ae47a0d
Oliver Davies <oliver@oliverdavies.dev>
2025-01-03 18:00:00 +0000

    Re-add Phpactor

--- nix/modules/home-manager/features/cli/neovim.nix
+++ nix/modules/home-manager/features/cli/neovim.nix
@@ -24,6 +24,7 @@ with lib;
         mini-nvim
         nvim-web-devicons
         oil-nvim
+        phpactor
         refactoring-nvim
         standard-vim
         undotree
---
+++ nvim/plugin/phpactor.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>pm", "<cmd>PhpactorContextMenu<CR>")
+vim.keymap.set("n", "<leader>pn", "<cmd>PhpactorClassNew<CR>")
