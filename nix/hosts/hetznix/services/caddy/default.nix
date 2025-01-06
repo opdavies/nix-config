2a55da9eb8feb9550cfbdbda6b54e83ee9b588f4
Oliver Davies <oliver@oliverdavies.dev>
2025-01-06 06:43:11 +0000

    Move vhost imports

--- nix/hosts/hetznix/services/caddy/default.nix
+++ nix/hosts/hetznix/services/caddy/default.nix
@@ -1,20 +1,5 @@
 {
   services.caddy.enable = true;
 
-  imports = [
-    ./vhosts/2020.oliverdavies.uk.nix
-    ./vhosts/bootstrap-with-tailwind.oliverdavies.uk.nix
-    ./vhosts/florida-drupalcamp-tailwind-css.oliverdavies.uk.nix
-    ./vhosts/luke.oliverdavies.uk.nix
-    ./vhosts/phpsw-sculpin-demo.oliverdavies.uk.nix
-    ./vhosts/rebuilding-acquia.oliverdavies.uk.nix
-    ./vhosts/rebuilding-bartik.oliverdavies.uk.nix
-    ./vhosts/rebuilding-bristol-js.oliverdavies.uk.nix
-    ./vhosts/rebuilding-symfony.oliverdavies.uk.nix
-    ./vhosts/tailwindcss-demo.oliverdavies.uk.nix
-    ./vhosts/talking-drupal-tailwindcss.oliverdavies.uk.nix
-    ./vhosts/wp-tailwind.oliverdavies.uk.nix
-    ./vhosts/www.oliverdavies.uk.nix
-    ./vhosts/zet.oliverdavies.uk.nix
-  ];
+  imports = [ ./vhosts ];
 }
---
+++ nix/hosts/hetznix/services/caddy/vhosts/default.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ./2020.oliverdavies.uk.nix
+    ./bootstrap-with-tailwind.oliverdavies.uk.nix
+    ./florida-drupalcamp-tailwind-css.oliverdavies.uk.nix
+    ./luke.oliverdavies.uk.nix
+    ./phpsw-sculpin-demo.oliverdavies.uk.nix
+    ./rebuilding-acquia.oliverdavies.uk.nix
+    ./rebuilding-bartik.oliverdavies.uk.nix
+    ./rebuilding-bristol-js.oliverdavies.uk.nix
+    ./rebuilding-symfony.oliverdavies.uk.nix
+    ./tailwindcss-demo.oliverdavies.uk.nix
+    ./talking-drupal-tailwindcss.oliverdavies.uk.nix
+    ./wp-tailwind.oliverdavies.uk.nix
+    ./www.oliverdavies.uk.nix
+    ./zet.oliverdavies.uk.nix
+  ];
+}
