fefdf04a6d28ceb933989d47bdbb59a3cfeeedda
Oliver Davies <oliver@oliverdavies.dev>
2025-01-12 11:40:45 +0000

    Use a wildcard certificate

--- nix/hosts/hetznix/security/acme.nix
+++ nix/hosts/hetznix/security/acme.nix
@@ -14,25 +14,7 @@
     certs."oliverdavies.uk" = {
       domain = "oliverdavies.uk";
 
-      extraDomainNames = [
-        # TODO Refactor to use a wildcard certificate.
-        "2020.oliverdavies.uk"
-        "bootstrap-with-tailwind.oliverdavies.uk"
-        "code.oliverdavies.uk"
-        "florida-drupalcamp-tailwind-css.oliverdavies.uk"
-        "luke.oliverdavies.uk"
-        "phpsw-sculpin-demo.oliverdavies.uk"
-        "rebuilding-acquia.oliverdavies.uk"
-        "rebuilding-bartik.oliverdavies.uk"
-        "rebuilding-bristol-js.oliverdavies.uk"
-        "rebuilding-symfony.oliverdavies.uk"
-        "tailwindcss-demo.oliverdavies.uk"
-        "talking-drupal-tailwindcss.oliverdavies.uk"
-        "tome.oliverdavies.uk"
-        "wp-tailwind.oliverdavies.uk"
-        "www.oliverdavies.uk"
-        "zet.oliverdavies.uk"
-      ];
+      extraDomainNames = [ "*.oliverdavies.uk" ];
     };
   };
 }
