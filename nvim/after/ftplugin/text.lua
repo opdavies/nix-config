d3ac84b6ddc90932271eb889525285d13bc44857
Oliver Davies <oliver@oliverdavies.dev>
2025-01-19 23:37:30 +0000

    Automatically add a blank line to the end of text
    ...files

--- nvim/after/ftplugin/text.lua
+++ nvim/after/ftplugin/text.lua
@@ -4,3 +4,14 @@ opt.number = false
 opt.relativenumber = false
 opt.spell = true
 opt.wrap = true
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = 0,
+  callback = function()
+    local last_line = vim.api.nvim_buf_get_lines(0, -2, -1, false)[1]
+
+    if last_line ~= "" then
+      vim.api.nvim_buf_set_lines(0, -1, -1, false, { "" })
+    end
+  end,
+})
