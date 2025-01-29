fe4f07f8eeefac1c2cb29c409d9731a27bbc4c51
Oliver Davies <oliver@oliverdavies.dev>
2025-01-29 01:49:03 +0000

    Simplify imports

--- nix/modules/home-manager/features/cli/default.nix
+++ nix/modules/home-manager/features/cli/default.nix
@@ -1,43 +1,19 @@
-{
-  config,
-  inputs,
-  lib,
-  pkgs,
-  self,
-  ...
-}:
+{ ... }:
 
 {
   imports = [
-    (import ./neovim.nix {
-      inherit
-        config
-        inputs
-        lib
-        pkgs
-        self
-        ;
-    })
-
-    (import ./tmux-sessionizer.nix {
-      inherit
-        config
-        lib
-        pkgs
-        self
-        ;
-    })
-
     ./bin.nix
     ./direnv.nix
     ./fzf.nix
     ./git.nix
+    ./neovim.nix
     ./notes.nix
     ./phpactor.nix
     ./ripgrep.nix
     ./starship.nix
     ./syncthing.nix
     ./tmux.nix
+    ./tmux-sessionizer.nix
     ./zsh.nix
   ];
 }
