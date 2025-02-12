77621d00ef43f6fbd67b1ff2995795ec57785bf4
Oliver Davies <oliver@oliverdavies.dev>
2025-02-12 21:19:07 +0000

    Disable mini.hipatterns

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -4,13 +4,6 @@ require("mini.align").setup {}
 
 require("mini.bracketed").setup {}
 
-require("mini.hipatterns").setup {
-  highlighters = {
-    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-  },
-}
-
 require("mini.move").setup {}
 
 require("mini.operators").setup {}
