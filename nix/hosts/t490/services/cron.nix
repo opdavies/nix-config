1ca1ac3c4a4a72e5d28730e0e4aa4cee37f025ad
Oliver Davies <oliver@oliverdavies.dev>
2025-02-15 01:35:08 +0000

    Add a cron job to run mbsync twice a day

--- nix/hosts/t490/services/cron.nix
+++ nix/hosts/t490/services/cron.nix
@@ -6,6 +6,7 @@
 
     systemCronJobs = [
       "*/15 * * * * opdavies ${pkgs.imapfilter}/bin/imapfilter -c ~/Code/email-filters/config.lua"
+      "0 8,20 * * * opdavies ${pkgs.isync}/bin/mbsync -a"
     ];
   };
 }
