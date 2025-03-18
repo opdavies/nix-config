bce72f1e569f00163340ca96d07612923bfb319c
Oliver Davies <oliver@oliverdavies.dev>
2025-03-18 17:26:15 +0000

    Move .wget-hsts

--- nix/home/opdavies/default.nix
+++ nix/home/opdavies/default.nix
@@ -21,6 +21,7 @@ let
     ];
   };
 
+  inherit (config.xdg) cacheHome configHome;
   inherit (pkgs) lib;
 
   shared-config = import "${self}/nix/lib/shared/home-manager.nix" {
@@ -71,9 +72,16 @@ in
     ];
     PULUMI_SKIP_UPDATE_CHECK = "true";
     W3M_DIR = "${config.xdg.stateHome}/w3m";
+    WGETRC = "${configHome}/wgetrc";
     XDG_CONFIG_HOME = "${config.xdg.configHome}";
     XDG_DATA_HOME = "${config.xdg.dataHome}";
     XDG_DOCUMENTS_DIR = "$HOME/Documents";
     XDG_REPOS_DIR = "$HOME/Code";
+  };
+
+  xdg.configFile = {
+    "${config.home.sessionVariables.WGETRC}".text = ''
+      hsts-file = "${cacheHome}/wget-hsts"
+    '';
   };
 }
