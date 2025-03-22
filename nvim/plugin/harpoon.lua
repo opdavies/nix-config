6f2c2ad092039beabb5e8e9b598d2a637d50c245
Oliver Davies <oliver@oliverdavies.dev>
2025-03-22 22:57:59 +0000

    Update Harpoon configuration

--- nvim/plugin/harpoon.lua
+++ nvim/plugin/harpoon.lua
@@ -2,12 +2,28 @@ local harpoon = require "harpoon"
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>hm", function()
+vim.keymap.set("n", "<leader>a", function()
   harpoon:list():add()
 end)
 
-vim.keymap.set("n", "<leader>hl", function()
+vim.keymap.set("n", "<C-e>", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set("n", "<C-h>", function()
+  harpoon:list():select(1)
+end)
+
+vim.keymap.set("n", "<C-j>", function()
+  harpoon:list():select(2)
+end)
+
+vim.keymap.set("n", "<C-k>", function()
+  harpoon:list():select(3)
+end)
+
+vim.keymap.set("n", "<C-l>", function()
+  harpoon:list():select(4)
 end)
 
 -- Set <space>1..<space>5 be my shortcuts to moving to the files
