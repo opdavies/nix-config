874ede426ebbf09b7458d1be3632531dd640e48f
Oliver Davies <oliver@oliverdavies.dev>
2025-03-27 13:09:43 +0000

    Add keymaps for custom PHP macros
    Wrap the selected text with `var_dump()`, `dump()` or `dd()`.

---
+++ nvim/after/ftplugin/php.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("v", "<leader>dd", "yodd(['<Esc>pA', <Esc>pA]);<Esc>")
+vim.keymap.set("v", "<leader>du", "yodump(['<Esc>pA', <Esc>pA]);<Esc>")
+vim.keymap.set("v", "<leader>vd", "yovar_dump(['<Esc>pA', <Esc>pA]);<Esc>")
