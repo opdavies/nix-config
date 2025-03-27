d9f50762a72cf983fcbe30a9c1f1b82025e27897
Oliver Davies <oliver@oliverdavies.dev>
2025-03-27 20:30:53 +0000

    Clean shared Home Manager packages

--- nix/lib/shared/home-manager-packages.nix
+++ nix/lib/shared/home-manager-packages.nix
@@ -4,39 +4,21 @@
   username,
   ...
 }:
-let
-  php = pkgs.php82;
-  phpPackages = pkgs.php82Packages;
 
-  inherit (pkgs) writeShellApplication;
-
-  _timer = writeShellApplication (import ./scripts/_timer.nix);
-  deliver = writeShellApplication (import ./scripts/deliver.nix { inherit pkgs; });
-  export-video-list = writeShellApplication (
-    import ./scripts/export-video-list.nix { inherit pkgs username; }
-  );
-  run = writeShellApplication (import ./scripts/run.nix { inherit pkgs; });
-  timer = writeShellApplication (import ./scripts/timer.nix);
-in
 with pkgs;
 [
   cachix
   delta
-  devenv
-  doppler
   entr
   gcc
   git
   git-crypt
   gnupg
-  go
   inotify-tools
   jq
   killall
   lua
   mob
-  php
-  phpPackages.composer
   pv
   simple-http-server
   sshs
@@ -50,14 +32,4 @@ with pkgs;
   xclip
   xdg-utils
   zbar
-
-  # Scripts.
-  deliver
-  run
-]
-++ pkgs.lib.optionals (!headless) [
-  # Scripts.
-  _timer
-  export-video-list
-  timer
 ]
