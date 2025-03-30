3388fd2b7e6794ab053026a3dcba509b8d6079e4
Oliver Davies <oliver@oliverdavies.dev>
2025-03-30 17:21:19 +0100

    Add a keymap to easily add `{{site.url))/` before
    ...some text
    
    This is useful when writing my daily emails and creating links within
    the content.

--- modules/home-manager/cli/neovim/config/after/ftplugin/markdown.lua
+++ modules/home-manager/cli/neovim/config/after/ftplugin/markdown.lua
@@ -1,3 +1,5 @@
 -- TODO: Interim fix for https://github.com/nvim-treesitter/nvim-treesitter-context/issues/431.
 vim.opt_local.spell = true
 vim.opt_local.wrap = true
+
+vim.keymap.set("n", "<leader>su", "Bi{{site.url}}/<Esc>")
