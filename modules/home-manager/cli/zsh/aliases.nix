016bd767920dbf467168fc68c0b4d5ccde31f3a5
Oliver Davies <oliver@oliverdavies.dev>
2025-03-30 17:00:52 +0100

    Remove unused aliases

--- modules/home-manager/cli/zsh/aliases.nix
+++ modules/home-manager/cli/zsh/aliases.nix
@@ -8,8 +8,6 @@
   "...." = "cd ../../..";
   "....." = "cd ../../../..";
   run = "./run";
-  s = "secrets";
-  secrets = ''doppler --project "$(whoami)" run'';
   tag = "tag-release";
   vss = "LC_ALL=C sort --unique ${config.xdg.userDirs.extraConfig.XDG_REPOS_DIR}/nixos-config/modules/home-manager/cli/neovim/config/spell/en.utf-8.adddotfiles/nvim/spell/en.utf-8.add --output ${config.xdg.userDirs.extraConfig.XDG_REPOS_DIR}/nixos-config/modules/home-manager/cli/neovim/config/spell/en.utf-8.add";
   wt = "git worktree";
