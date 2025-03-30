640ef4a97e5032942b8a43267969f7671f4c2542
Oliver Davies <oliver@oliverdavies.dev>
2025-03-30 12:43:34 +0100

    Fix spellfile path

--- modules/home-manager/cli/zsh/aliases.nix
+++ modules/home-manager/cli/zsh/aliases.nix
@@ -10,6 +10,6 @@
   s = "secrets";
   secrets = ''doppler --project "$(whoami)" run'';
   tag = "tag-release";
-  vss = "LC_ALL=C sort --unique ${config.xdg.userDirs.extraConfig.XDG_REPOS_DIR}/dotfiles/nvim/spell/en.utf-8.add --output ${config.xdg.userDirs.extraConfig.XDG_REPOS_DIR}/dotfiles/nvim/spell/en.utf-8.add";
+  vss = "LC_ALL=C sort --unique ${config.xdg.userDirs.extraConfig.XDG_REPOS_DIR}/nixos-config/modules/home-manager/cli/neovim/config/spell/en.utf-8.adddotfiles/nvim/spell/en.utf-8.add --output ${config.xdg.userDirs.extraConfig.XDG_REPOS_DIR}/nixos-config/modules/home-manager/cli/neovim/config/spell/en.utf-8.add";
   wt = "git worktree";
 }
