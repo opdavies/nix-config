af3c7b2f2b1fddf6dec20e9f4ecea355a3d1b1ab
Oliver Davies <oliver@oliverdavies.dev>
2025-04-03 17:15:53 +0100

    Add wiki-push and wiki-pull scripts

--- home/opdavies/hosts/PW05CH3L.nix
+++ home/opdavies/hosts/PW05CH3L.nix
@@ -1,4 +1,11 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
   homeManagerModules = {
     direnv.enable = true;
     neovim.enable = true;
@@ -12,7 +19,34 @@
     tmux-sessionizer.enable = true;
   };
 
-  home.sessionVariables = {
-    WAYLAND_DISPLAY = "";
-  };
+  home =
+    let
+      remoteIpAddress = "192.168.1.111";
+      wikiPath = "${config.xdg.userDirs.documents}/wiki";
+    in
+    {
+      packages = [
+        (pkgs.writeShellApplication {
+          name = "wiki-pull";
+
+          text = ''
+            ${lib.getExe pkgs.rsync} -avz --update \
+              ${remoteIpAddress}:${wikiPath}/ ${wikiPath} "$@"
+          '';
+        })
+
+        (pkgs.writeShellApplication {
+          name = "wiki-push";
+
+          text = ''
+            ${lib.getExe pkgs.rsync} -avz --update \
+              ${wikiPath}/ ${remoteIpAddress}:${wikiPath} "$@"
+          '';
+        })
+      ];
+
+      sessionVariables = {
+        WAYLAND_DISPLAY = "";
+      };
+    };
 }
