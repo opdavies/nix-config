88a37556994023fc4b5ca4c0eadcfab1b6bde4a5
Oliver Davies <oliver@oliverdavies.dev>
2025-04-03 20:14:36 +0100

    Add wiki-push script

--- home/opdavies/hosts/t480.nix
+++ home/opdavies/hosts/t480.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   homeManagerModules = {
@@ -32,6 +32,28 @@
     displayselect
     isync
     upload-to-files
+
+    (pkgs.writeShellApplication (
+      let
+        remoteIpAddresses = [
+          "116.203.11.255"
+          "192.168.1.116"
+        ];
+
+        wikiPath = "${config.xdg.userDirs.documents}/wiki";
+      in
+      {
+        name = "wiki-push";
+
+        text = builtins.concatStringsSep "\n" (
+          map (ipAddress: ''
+            ${lib.getExe pkgs.rsync} -avz --update \
+              --delete --delete-after \
+              ${wikiPath}/ ${ipAddress}:${wikiPath} "$@"
+          '') remoteIpAddresses
+        );
+      }
+    ))
   ];
 
   programs.neomutt = {
