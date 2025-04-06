4836066be442f1f0fafc5d4528b26e29728281cb
Oliver Davies <oliver@oliverdavies.dev>
2025-04-06 01:00:57 +0100

    Combine default.nix and home.nix

--- home/opdavies/default.nix
+++ home/opdavies/default.nix
@@ -17,19 +17,48 @@ let
     ];
   };
 
+  inherit (pkgs) lib;
+
   inherit (config.xdg)
     cacheHome
     configHome
     dataHome
     stateHome
     ;
-
-  inherit (pkgs) lib;
 in
 {
-  imports = [
-    ./home.nix
+  home.username = "opdavies";
+  home.homeDirectory = "/home/${config.home.username}";
+
+  home.packages = with pkgs; [
+    cachix
+    delta
+    entr
+    gcc
+    git
+    git-crypt
+    gnupg
+    inotify-tools
+    jq
+    killall
+    lua
+    mob
+    pv
+    simple-http-server
+    sshs
+    tldr
+    tree
+    tree-sitter
+    unzip
+    w3m
+    watchexec
+    wget
+    xclip
+    xdg-utils
+    zbar
   ];
+
+  programs.home-manager.enable = true;
 
   home.sessionVariables = {
     EDITOR = "nvim";
@@ -67,4 +96,9 @@ in
       };
     };
   };
+
+  # Nicely reload system units when changing configs.
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "22.05"; # Please read the comment before changing.
 }
