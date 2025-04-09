d180f46e9c3f16c0eb10dacb5c0db7d40178d356
Oliver Davies <oliver@oliverdavies.dev>
2025-04-09 18:00:00 +0100

    Use YAML ft for phpstan.neon and phpstan.neon.dist

--- modules/editor/config/plugin/filetype.lua
+++ modules/editor/config/plugin/filetype.lua
@@ -3,11 +3,13 @@ vim.filetype.add {
     inc = "php",
     install = "php",
     module = "php",
+    neon = "yaml",
     pcss = "scss",
     theme = "php",
   },
 
   filename = {
     ["composer.lock"] = "json",
+    ["phpstan.neon.dist"] = "yaml",
   },
 }
