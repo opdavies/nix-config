ffef2a8f6f2eacfd1f600e01ce8bdfebfb8253c8
Oliver Davies <oliver@oliverdavies.dev>
2025-04-21 11:12:29 +0100

    Update sha256

--- pkgs/displayselect/default.nix
+++ pkgs/displayselect/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
     owner = "lukesmithxyz";
     repo = "voidrice";
     rev = "master";
-    sha256 = "u4iy2p9i5ORLHcKmAOHVJNaKfYbt+SYUYKblgsRevN8=";
+    sha256 = "sha256-9U1Do0w2oT5E6uZxSKoHAzbGbSLQRQlT65KcPGzwhW8=";
   };
 
   buildInputs = [
