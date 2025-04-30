e6f269b12348774a7f4787f8f6f673c2e4d36a15
Oliver Davies <oliver@oliverdavies.dev>
2025-04-30 01:27:56 +0100

    Refactor Nginx vhosts and ports

---
+++ hosts/nixedo/modules/nginx/ports.nix
@@ -0,0 +1,15 @@
+{
+  bootstrap-with-tailwind = 8081;
+  eric = 8084;
+  florida-drupalcamp-tailwind-css = 8083;
+  luke = 8094;
+  phpsw-sculpin-demo = 8085;
+  rebuilding-acquia = 8086;
+  rebuilding-bartik = 8087;
+  rebuilding-bristol-js = 8088;
+  rebuilding-symfony = 8089;
+  tailwindcss-demo = 8090;
+  talking-drupal-tailwindcss = 8093;
+  wp-tailwind = 8091;
+  zet = 8092;
+}
--- hosts/nixedo/modules/nginx/sites.nix
+++ hosts/nixedo/modules/nginx/sites.nix
@@ -1,87 +1,35 @@
-[
-  {
-    url = "bootstrap-with-tailwind.oliverdavies.uk";
-    root = "/var/www/vhosts/bootstrap-with-tailwind";
-    port = 8081;
-  }
+let
+  baseDomain = "oliverdavies.uk";
 
-  {
-    port = 8084;
-    root = "/var/www/vhosts/eric/public";
-    url = "eric.oliverdavies.uk";
+  ports = import ./ports.nix;
 
-    extraConfig = ''
-      add_header X-Robots-Tag "noindex, nofollow";
-    '';
-  }
+  mkSite =
+    name: overrides:
+    let
+      root = "/var/www/vhosts/${name}" + (overrides.rootSuffix or "");
+    in
+    {
+      inherit root;
 
-  {
-    port = 8083;
-    root = "/var/www/vhosts/florida-drupalcamp-tailwind-css";
-    url = "florida-drupalcamp-tailwind-css.oliverdavies.uk";
-  }
+      port = ports.${name};
+      url = "${name}.${baseDomain}";
+    }
+    // overrides;
 
-  {
-    port = 8094;
-    root = "/var/www/vhosts/luke";
-    url = "luke.oliverdavies.uk";
+  sites = [
+    (mkSite "eric" {
+      rootSuffix = "/public";
 
-    extraConfig = ''
-      add_header X-Robots-Tag "noindex, nofollow";
-    '';
-  }
+      extraConfig = ''
+        add_header X-Robots-Tag "noindex, nofollow";
+      '';
+    })
 
-  {
-    port = 8085;
-    root = "/var/www/vhosts/phpsw-sculpin-demo";
-    url = "phpsw-sculpin-demo.oliverdavies.uk";
-  }
-
-  {
-    port = 8086;
-    root = "/var/www/vhosts/rebuilding-acquia";
-    url = "rebuilding-acquia.oliverdavies.uk";
-  }
-
-  {
-    port = 8087;
-    root = "/var/www/vhosts/rebuilding-bartik";
-    url = "rebuilding-bartik.oliverdavies.uk";
-  }
-
-  {
-    port = 8088;
-    root = "/var/www/vhosts/rebuilding-bristol-js";
-    url = "rebuilding-bristol-js.oliverdavies.uk";
-  }
-
-  {
-    port = 8089;
-    root = "/var/www/vhosts/rebuilding-symfony";
-    url = "rebuilding-symfony.oliverdavies.uk";
-  }
-
-  {
-    port = 8090;
-    root = "/var/www/vhosts/tailwindcss-demo";
-    url = "tailwindcss-demo.oliverdavies.uk";
-  }
-
-  {
-    port = 8091;
-    root = "/var/www/vhosts/wp-tailwind";
-    url = "wp-tailwind.oliverdavies.uk";
-  }
-
-  {
-    port = 8092;
-    root = "/var/www/vhosts/zet";
-    url = "zet.oliverdavies.uk";
-  }
-
-  {
-    port = 8093;
-    root = "/var/www/vhosts/talking-drupal-tailwindcss";
-    url = "talking-drupal-tailwindcss.oliverdavies.uk";
-  }
-]
+    (mkSite "luke" {
+      extraConfig = ''
+        add_header X-Robots-Tag "noindex, nofollow";
+      '';
+    })
+  ];
+in
+sites
