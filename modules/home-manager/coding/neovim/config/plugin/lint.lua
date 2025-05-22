6e4c690e5d5b6212f351cc503dd05707b811abb0
Oliver Davies <oliver@oliverdavies.dev>
2025-05-22 08:38:49 +0100

    Remove global formatters and linters
    Following commit 724753327, this removes all global formatters and
    linters so I can move them into project-specific flake.nix and .nvim.lua
    files.

--- modules/home-manager/coding/neovim/config/plugin/lint.lua
+++ modules/home-manager/coding/neovim/config/plugin/lint.lua
@@ -1,14 +1,7 @@
 local lint = require "lint"
 
-lint.linters_by_ft = {
-  dockerfile = { "hadolint" },
-  javascript = { "eslint_d" },
-  json = { "jsonlint" },
-  lua = { "luacheck" },
-  markdown = { "markdownlint" },
-  nix = { "nix" },
-  php = { "php", "phpcs", "phpstan" },
-}
+lint.linters_by_ft = {};
+
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
