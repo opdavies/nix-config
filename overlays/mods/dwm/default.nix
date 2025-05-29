8f5acb2fdb151ae3652db1955d7c05d50adb26ca
Oliver Davies <oliver@oliverdavies.dev>
2025-05-29 17:00:38 +0100

    Move keybinds from dwm to sxhkd

--- overlays/mods/dwm/default.nix
+++ overlays/mods/dwm/default.nix
@@ -89,17 +89,11 @@ prev.dwm.override {
       TAGKEYS(XK_7, 6)
       TAGKEYS(XK_8, 7)
       TAGKEYS(XK_9, 8)
-      { MODKEY|ShiftMask, XK_b, spawn, {.v = (const char*[]){ "/etc/profiles/per-user/opdavies/bin/bookmarkthis", NULL }}},
       { MODKEY|ShiftMask, XK_f, spawn, SHCMD("${prev.xfce.thunar}/bin/thunar") },
-      { MODKEY|ShiftMask, XK_i, spawn, SHCMD("${prev.xdotool}/bin/xdotool type $(cat ~/.local/share/snippets.txt | grep -v '^#' | grep -v '^$' | sort | dmenu -i -l 50 | cut -d' ' -f1)") },
-      { MODKEY|ShiftMask, XK_p, spawn, {.v = (const char*[]){ "${lib.getExe prev.passmenu-otp}", "-i", NULL} } },
       { MODKEY|ShiftMask, XK_q, quit, {0} },
       { MODKEY|ShiftMask, XK_r, quit, {1} },
       { MODKEY|ShiftMask, XK_s, spawn, SHCMD("${prev.flameshot}/bin/flameshot gui") },
       { MODKEY, XK_w, spawn, SHCMD(BROWSER) },
-      { MODKEY, XK_F3, spawn, {.v = (const char*[]){ "${lib.getExe prev.displayselect}", NULL} } },
-      { MODKEY, XK_F9, spawn, {.v = (const char*[]){ "/home/opdavies/.local/bin/mounter", NULL } } },
-      { MODKEY, XK_F10, spawn, {.v = (const char*[]){ "/home/opdavies/.local/bin/unmounter", NULL } } },
     };
 
     static const Button buttons[] = {
