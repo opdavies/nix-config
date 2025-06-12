e65f3e42a52d3128526c77df315e9457acd038c5
Oliver Davies <oliver@oliverdavies.uk>
2025-06-12 12:13:49 +0100

    Re-add LSP keymaps

--- modules/home-manager/coding/neovim/plugins/lsp.nix
+++ modules/home-manager/coding/neovim/plugins/lsp.nix
@@ -1,3 +1,19 @@
 {
   plugins.lsp.enable = true;
+
+  plugins.lsp.keymaps = {
+    diagnostic = {
+      "<leader>dl" = "setqflist";
+    };
+
+    lspBuf = {
+      "<leader>ca" = "code_action";
+      "<leader>cr" = "rename";
+      K = "hover";
+      gD = "declaration";
+      gT = "type_definition";
+      gd = "definition";
+      gr = "references";
+    };
+  };
 }
