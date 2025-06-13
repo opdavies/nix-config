aee90917d94120577d39e1c6753f517c9b8fc4e6
Oliver Davies <oliver@oliverdavies.uk>
2025-06-13 01:36:54 +0100

    Don't automatically save/close Harpoon windows

--- modules/home-manager/coding/neovim/config/plugins/auto-save.nix
+++ modules/home-manager/coding/neovim/config/plugins/auto-save.nix
@@ -1,3 +1,21 @@
 {
   plugins.auto-save.enable = true;
+
+  plugins.auto-save.settings.condition = ''
+    function(buf)
+      local excluded_filetypes = {
+        "harpoon"
+      }
+
+      local excluded_filenames = {}
+
+      if vim.tbl_contains(excluded_filetypes, vim.fn.getbufvar(buf, "&filetype"))
+        or vim.tbl_contains(excluded_filenames, vim.fn.expand("%:t"))
+      then
+        return false
+      end
+
+      return true
+    end
+  '';
 }
