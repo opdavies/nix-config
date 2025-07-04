8b37a1fa193efee7b8ac975f2c44ad477a6cf410
Oliver Davies <oliver@oliverdavies.uk>
2025-07-04 10:01:32 +0100

    Re-add missing demo/example websites

--- hosts/nixedo/modules/nginx/sites.nix
+++ hosts/nixedo/modules/nginx/sites.nix
@@ -10,30 +10,48 @@ let
     in
     { inherit port root url; } // overrides;
 
-  sites = [
-    {
-      root = "/var/www/vhosts/website-sculpin";
-      port = ports.nginx-website-2025;
-      url = "2025.oliverdavies.uk";
+  sites =
+    [
+      {
+        root = "/var/www/vhosts/website-sculpin";
+        port = ports.nginx-website-2025;
+        url = "2025.oliverdavies.uk";
 
-      extraConfig = ''
-        add_header X-Robots-Tag "noindex, nofollow";
-      '';
-    }
+        extraConfig = ''
+          add_header X-Robots-Tag "noindex, nofollow";
+        '';
+      }
 
-    (mkSite "eric" {
-      rootSuffix = "/public";
+      (mkSite "eric" {
+        rootSuffix = "/public";
 
-      extraConfig = ''
-        add_header X-Robots-Tag "noindex, nofollow";
-      '';
-    })
-
-    (mkSite "luke" {
-      extraConfig = ''
-        add_header X-Robots-Tag "noindex, nofollow";
-      '';
-    })
-  ];
+        extraConfig = ''
+          add_header X-Robots-Tag "noindex, nofollow";
+        '';
+      })
+    ]
+    ++ map
+      (
+        domain:
+        mkSite domain {
+          extraConfig = ''
+            add_header X-Robots-Tag "noindex, nofollow";
+          '';
+        }
+      )
+      [
+        "bootstrap-with-tailwind"
+        "florida-drupalcamp-tailwind-css"
+        "luke"
+        "phpsw-sculpin-demo"
+        "rebuilding-acquia"
+        "rebuilding-bartik"
+        "rebuilding-bristol-js"
+        "rebuilding-symfony"
+        "tailwindcss-demo"
+        "talking-drupal-tailwindcss"
+        "wp-tailwind"
+        "zet"
+      ];
 in
 sites
