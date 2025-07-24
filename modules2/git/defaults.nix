c77d7e4e5508f76d994966a8ce4fdf5c2394a2d6
Oliver Davies <oliver@oliverdavies.uk>
2025-07-24 23:40:59 +0100

    Refactor

--- modules2/git/defaults.nix
+++ modules2/git/defaults.nix
@@ -1,64 +1,59 @@
 { config, ... }:
 
-let
-  cfg = config;
-in
 {
-  flake.modules.homeManager.base =
-    { config, ... }:
-    {
-      programs.git = {
-        userName = cfg.flake.meta.owner.name;
-        userEmail = cfg.flake.meta.owner.email;
+  flake.modules.homeManager.base = hmArgs: {
+    programs.git = {
+      userName = config.flake.meta.owner.name;
+      userEmail = config.flake.meta.owner.email;
 
-        extraConfig = {
-          branch = {
-            autosetupmerge = true;
-            autosetuprebase = "always";
-            sort = "-committerdate";
-          };
-
-          checkout.defaultRemote = "origin";
-          color.ui = true;
-          column.ui = "auto";
-          commit.template = "${config.xdg.configHome}/git/message";
-
-          core = {
-            editor = "nvim";
-            excludesFile = "~/.config/git/ignore";
-          };
-
-          diff.tool = "vimdiff";
-          fetch.prune = true;
-          gpg.format = "ssh";
-          grep.lineNumber = true;
-          help.autocorrect = "1";
-          init.defaultBranch = "main";
-
-          maintenance = {
-            auto = false;
-            strategy = "incremental";
-          };
-
-          merge.ff = "only";
-
-          push = {
-            autoSetupRemote = true;
-            default = "upstream";
-          };
-
-          pull = {
-            ff = "only";
-            rebase = true;
-          };
-
-          rebase = {
-            autosquash = true;
-            autostash = true;
-          };
-
-          user.signingkey = "~/.ssh/id_rsa.pub";
+      extraConfig = {
+        branch = {
+          autosetupmerge = true;
+          autosetuprebase = "always";
+          sort = "-committerdate";
         };
+
+        checkout.defaultRemote = "origin";
+        color.ui = true;
+        column.ui = "auto";
+        commit.template = "${hmArgs.config.xdg.configHome}/git/message";
+
+        core = {
+          editor = "nvim";
+          excludesFile = "~/.config/git/ignore";
+        };
+
+        diff.tool = "vimdiff";
+        fetch.prune = true;
+        gpg.format = "ssh";
+        grep.lineNumber = true;
+        help.autocorrect = "1";
+        init.defaultBranch = "main";
+
+        maintenance = {
+          auto = false;
+          strategy = "incremental";
+        };
+
+        merge.ff = "only";
+
+        push = {
+          autoSetupRemote = true;
+          default = "upstream";
+        };
+
+        pull = {
+          ff = "only";
+          rebase = true;
+        };
+
+        rebase = {
+          autosquash = true;
+          autostash = true;
+        };
+
+        user.signingkey = "~/.ssh/id_rsa.pub";
       };
     };
+  };
 }
