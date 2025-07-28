4eead1485f228e207f33d4b7f1efb7b8c6420a5a
Oliver Davies <oliver@oliverdavies.uk>
2025-07-28 19:38:17 +0100

    Refactor inputs

--- flake.nix
+++ flake.nix
@@ -1,47 +1,35 @@
 {
   inputs = {
-    agenix = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:ryantm/agenix";
-    };
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.url = "github:ryantm/agenix";
 
-    disko = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/disko";
-    };
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko.url = "github:nix-community/disko";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/master";
-    };
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/master";
 
     import-tree.url = "github:vic/import-tree";
 
-    nix-index-database = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/nix-index-database";
-    };
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database.url = "github:nix-community/nix-index-database";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
-    nixvim = {
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/nixvim";
-    };
+    nixvim.inputs.flake-parts.follows = "flake-parts";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:nix-community/nixvim";
 
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nur = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/NUR";
-    };
+    nur.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/NUR";
   };
 
   outputs =
