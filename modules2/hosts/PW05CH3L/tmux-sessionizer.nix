491aeb0c7e02194cb672495b9f5fa36de1feb286
Oliver Davies <oliver@oliverdavies.uk>
2025-07-28 19:38:17 +0100

    tmux-sessionizer: configure paths

---
+++ modules2/hosts/PW05CH3L/tmux-sessionizer.nix
@@ -0,0 +1,15 @@
+{ config, ... }:
+
+{
+  flake.modules.nixos."hosts/PW05CH3L".home-manager.users.${config.flake.meta.owner.username} =
+    args: {
+      programs.tmux-sessionizer.searchPaths =
+        let
+          inherit (args.config.xdg) userDirs;
+        in
+        [
+          "${userDirs.extraConfig.XDG_REPOS_DIR}:3"
+          "${userDirs.documents}:1"
+        ];
+    };
+}
--- modules2/hosts/t480/tmux-sessionizer.nix
+++ modules2/hosts/t480/tmux-sessionizer.nix
@@ -1,15 +1,15 @@
+{ config, ... }:
+
 {
-  flake.modules.homeManager.base =
-    { config, ... }:
-    {
-      programs.tmux-sessionizer.searchPaths =
-        let
-          inherit (config.xdg) userDirs;
-        in
-        [
-          "${userDirs.extraConfig.XDG_REPOS_DIR}:2"
-          "${userDirs.documents}:1"
-          "${config.home.homeDirectory}/tmp"
-        ];
-    };
+  flake.modules.nixos."hosts/t480".home-manager.users.${config.flake.meta.owner.username} = args: {
+    programs.tmux-sessionizer.searchPaths =
+      let
+        inherit (args.config.xdg) userDirs;
+      in
+      [
+        "${userDirs.extraConfig.XDG_REPOS_DIR}:2"
+        "${userDirs.documents}:1"
+        "${args.config.home.homeDirectory}/tmp"
+      ];
+  };
 }
