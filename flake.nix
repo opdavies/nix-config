3c798cba519243a7ae484109ca60a52a33f65680
Oliver Davies <oliver@oliverdavies.uk>
2025-07-30 18:00:00 +0100

    Refactor inputs

--- flake.nix
+++ flake.nix
@@ -1,47 +1,67 @@
 {
   inputs = {
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
-    agenix.url = "github:ryantm/agenix";
+    agenix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:ryantm/agenix";
+    };
 
-    conf-vim.flake = false;
-    conf-vim.url = "github:tjdevries/conf.vim";
+    conf-vim = {
+      flake = false;
+      url = "github:tjdevries/conf.vim";
+    };
 
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-    disko.url = "github:nix-community/disko";
+    disko = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/disko";
+    };
 
-    edit-alternate-vim.flake = false;
-    edit-alternate-vim.url = "github:tjdevries/edit_alternate.vim";
+    edit-alternate-vim = {
+      flake = false;
+      url = "github:tjdevries/edit_alternate.vim";
+    };
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/master";
+    };
 
     import-tree.url = "github:vic/import-tree";
 
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nix-index-database";
+    };
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
-    nixvim.inputs.flake-parts.follows = "flake-parts";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim = {
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixvim";
+    };
 
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nur.inputs.nixpkgs.follows = "nixpkgs";
-    nur.url = "github:nix-community/NUR";
+    nur = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/NUR";
+    };
 
-    standard-vim.flake = false;
-    standard-vim.url = "github:tjdevries/standard.vim";
+    standard-vim = {
+      flake = false;
+      url = "github:tjdevries/standard.vim";
+    };
 
-    vim-textobj-xmlattr.flake = false;
-    vim-textobj-xmlattr.url = "github:whatyouhide/vim-textobj-xmlattr";
+    vim-textobj-xmlattr = {
+      flake = false;
+      url = "github:whatyouhide/vim-textobj-xmlattr";
+    };
   };
 
   outputs =
